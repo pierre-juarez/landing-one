3403421408d5b5c045d3d1275980fd34d5f67704
Pierre Juarez U. <76144339+pierre-juarez@users.noreply.github.com>
2025-03-07 00:07:31 -0500

    [fix] - Visibility overflow

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
           "linear-gradient(240deg, rgba(37,69,244,0.5) 0%, rgba(255,255,255,0) 100%)",
       }}
     >
-      <div className="w-[100%] h-[100%]">
+      <div className="w-[100%] h-[776px]">
         <Navbar />
         <Banner />
       </div>
